aac67cfcdcbe1fec3378bea16ab9bc2fad483e0a
Mohanad-Shawky <mohanadshaw2y@gmail.com>
2024-02-23 19:03:58 +0200

    Add project files.

---
+++ RandomExamGenerator.WinFormsUI/Form1.cs
@@ -0,0 +1,10 @@
+namespace RandomExamGenerator.WinFormsUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
